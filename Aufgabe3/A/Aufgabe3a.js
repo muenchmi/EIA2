9185d7bbada6494df550cf306730dc27110e0af6
Michelle <Michelle@Michelles-PC.hfu-open.fu.hs-furtwangen.de>
2017-04-09 14:32:52 +0200

    3a

--- Aufgabe3/A/Aufgabe3a.js
+++ Aufgabe3/A/Aufgabe3a.js
@@ -1,1 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let rice = 1;
+    let zeile = 0;
+    let n = 64;
+    for (let i = 0; i < n; i++) {
+        let element = document.createElement("div");
+        element.textContent = rice.toString();
+        //Umbruch
+        if (i % 8 == 0) {
+            zeile = zeile + 1;
+        }
+        //Schwarzwei�
+        if (zeile % 2 == 0) {
+            if (i % 2 != 0) {
+                element.className = "board white";
+            }
+            else {
+                element.className = "board black";
+            }
+        }
+        else {
+            if (i % 2 != 0) {
+                element.className = "board black";
+            }
+            else {
+                element.className = "board white";
+            }
+        }
+        rice = rice * 2;
+        document.body.appendChild(element);
+    }
+});
 //# sourceMappingURL=Aufgabe3a.js.map
---
+++ Aufgabe3/A/Aufgabe3a.ts
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function(): void {
+    let rice: number = 1;
+    let zeile: number = 0;
+    let n: number = 64;
+    for (let i: number = 0; i < n; i++) {
+        let element: HTMLElement = document.createElement("div");
+        element.textContent = rice.toString();
+
+        
+        //Umbruch
+        if (i % 8 == 0) {
+            zeile = zeile + 1;
+        }
+        //Schwarzweiß
+        if (zeile % 2 == 0) {
+            if (i % 2 != 0) {
+                element.className = "board white";
+            }
+            else {
+                element.className = "board black";
+            }
+        }
+           else {
+            if (i % 2 != 0) {
+                element.className = "board black";
+            }
+            else {
+                element.className = "board white";
+            }
+        }
+        rice = rice * 2;
+        document.body.appendChild(element);
+    }
+});
