a9439f123a8c2c053a320531c4d6735d92bfa778
Michelle <Michelle@Michelles-PC.hfu-open.fu.hs-furtwangen.de>
2017-04-09 14:50:38 +0200

    3b

--- Aufgabe3/B/Aufgabe3b.js
+++ Aufgabe3/B/Aufgabe3b.js
@@ -1,1 +1,20 @@
+//<!-- 
+//Aufgabe: Aufgabe 3b
+//Name: Michelle M�nch
+//Matrikel:  254180 
+//Datum: 09.04.2017
+//    
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+//-->
+document.addEventListener("DOMContentLoaded", function () {
+    let cart = ["Herz As",
+        "Herz K�nig",
+        "Herz Dame",
+        "Herz Bube",
+        "Herz 10",
+        "Herz 9",
+        "Herz 8",
+        "Herz 7",
+    ];
+});
 //# sourceMappingURL=Aufgabe3b.js.map
---
+++ Aufgabe3/B/Aufgabe3b.ts
@@ -0,0 +1,21 @@
+//<!-- 
+//Aufgabe: Aufgabe 3b
+//Name: Michelle Münch
+//Matrikel:  254180 
+//Datum: 09.04.2017
+//    
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+//-->
+document.addEventListener("DOMContentLoaded", function(): void{
+let cart: string[] = ["Herz As",
+"Herz König",
+"Herz Dame",
+"Herz Bube",
+"Herz 10",
+"Herz 9",
+"Herz 8",
+"Herz 7",
+
+]
+    
+});
