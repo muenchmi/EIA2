a2fbdd2d9e84e7c5de3fd2ea7c70bf12f1815cf0
Michelle <Michelle@Michelles-PC.hfu-open.fu.hs-furtwangen.de>
2017-04-09 16:17:23 +0200

    3b

--- Aufgabe3/B/Aufgabe3b.ts
+++ Aufgabe3/B/Aufgabe3b.ts
@@ -6,74 +6,76 @@
 //    
 //Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 //-->
-document.addEventListener("DOMContentLoaded", function(): void{
-let karten: string[] = ["Herz As",
-"Herz König",
-"Herz Dame",
-"Herz Bube",
-"Herz 10",
-"Herz 9",
-"Herz 8",
-"Herz 7",
-"Karo As",
-"Karo König",
-"Karo Dame",
-"Karo Bube",
-"Karo 10",
-"Karo 9",
-"Karo 8",
-"Karo 7",
-"Pik As",
-"Pik König",
-"Pik Dame",
-"Pik Bube",
-"Pik 10",
-"Pik 9",
-"Pik 8",
-"Pik 7",
-"Kreuz As",
-"Kreuz König",
-"Kreuz Bube",
-"Kreuz Dame",
-"Kreuz 10",
-"Kreuz 9",
-"Kreuz 8",
-"Kreuz 7"
-];
- // Karten Text
-    div.innerText = karten[n];   
- //Karten vom Stapel in die Hand 
-    function nimmeinekarte(): void {
-        let div: HTMLDivElement = document.createElement("div");
-    document.getElementsByClassName("hkarten").appendChild(div);
-        //zufälligeKarte
-    let n: number = Math.round(Math.random()* karten.length);
-    }
-    
- //(max. 5)
-    let hand: HTMLElement= document.getElementsByClassName("hand");
-    hand.addEventListener("click", function(): void {
-        //max 5
-        let kartenaufderhand: number = document.getElementsByClassName("hkarten");
-        if (kartenaufderhand < 5) {
-            nimmeinekarte();}
- //Karten weglegen
-     div.addEventListener("click", function(): void {
-         let kartenweg: string [] = [];
+namespace Aufgabe3b {
+    let khand: NodeListOf<HTMLDivElement>;  // Zugriff auf diese div Elemente auf Hand
+    document.addEventListener("DOMContentLoaded", function(): void {
+        let karten: string[] = ["Herz As",
+            "Herz König",
+            "Herz Dame",
+            "Herz Bube",
+            "Herz 10",
+            "Herz 9",
+            "Herz 8",
+            "Herz 7",
+            "Karo As",
+            "Karo König",
+            "Karo Dame",
+            "Karo Bube",
+            "Karo 10",
+            "Karo 9",
+            "Karo 8",
+            "Karo 7",
+            "Pik As",
+            "Pik König",
+            "Pik Dame",
+            "Pik Bube",
+            "Pik 10",
+            "Pik 9",
+            "Pik 8",
+            "Pik 7",
+            "Kreuz As",
+            "Kreuz König",
+            "Kreuz Bube",
+            "Kreuz Dame",
+            "Kreuz 10",
+            "Kreuz 9",
+            "Kreuz 8",
+            "Kreuz 7"
+        ];
+
+        //(max. 5)
+        let deck: HTMLElement = document.getElementsByClassName("deck"); // HTML div Elemente
+        deck.addEventListener("click", function(): void {
+            //max 5
+            let kartenaufderhand: number = document.getElementsByClassName("hkarten");
+            if (kartenaufderhand < 5) {
+                nimmeinekarte();
+            }
+            //Karten vom Stapel in die Hand 
+            function nimmeinekarte(): void {
+                let div: HTMLDivElement = document.createElement("div");
+           document.getElementsByClassName("hkarten").appendChild(div);
+            //zufälligeKarte
+            let n: number = Math.round(Math.random() * karten.length);
+        }
+
+       //Karten weglegen
+      div.addEventListener("click", function(): void {
+         let kartenweg: string[] = [];
          let kartenwert: string = div.innerText;
          let kartenhaufen: number = kartenweg.push(kartenwert);
-         
-         document.getElementByClassName("weg").innerText= kartenwert;
-         
- //Divlöschen
-         div.parentNode.removeChild(div);
-     });
-          
-         
-       }); 
-            
-   
-    
-    
-    
-   
+
+         document.getElementByClassName("weg").innerText = kartenwert;
+                  //Divlöschen
+            div.parentNode.removeChild(div);
+        });
+    }
+//});
+
+
+
+
+
+
+
+
