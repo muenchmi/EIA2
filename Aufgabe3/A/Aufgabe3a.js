ac409ef81fe2333776dc128beac30746f4db24e0
Michelle <Michelle@Michelles-PC.hfu-open.fu.hs-furtwangen.de>
2017-04-09 16:34:11 +0200

    3a

--- Aufgabe3/A/Aufgabe3a.js
+++ Aufgabe3/A/Aufgabe3a.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (i % 8 == 0) {
             zeile = zeile + 1;
         }
-        //Schwarzwei�
+        //Schwarzweiß
         if (zeile % 2 == 0) {
             if (i % 2 != 0) {
                 element.className = "board white";
@@ -29,5 +29,34 @@ document.addEventListener("DOMContentLoaded", function () {
         rice = rice * 2;
         document.body.appendChild(element);
     }
+    var obereDivReihe = document.getElementsByTagName("div");
+    // Gibt den Feldern in der ersten Reihe bei einem Klick die Klasse "farbe", wodurch sie rot werden
+    for (var i = 0; i < 9; i++) {
+        obereDivReihe[i].addEventListener("click", function () {
+            this.classList.toggle("farbe"); //f�rbt genau dieses angeklickte div durch die cssKlasse farbe ein
+            summeK(); // hier wird der funktion summeK bei aktivierung gesagt welches div angeklickt worden ist
+        });
+    }
+    var roteDivs = document.getElementsByClassName("farbe");
+    var summe = 0; // wird die summe aller angeklickten divs nachher ergeben 
+    var toolTip = document.getElementById("tooltip"); // div im html mit id wo die summe der angeklickten divs nacher angezeigt wird
+    function summeK() {
+        if (roteDivs.length == 0) {
+            toolTip.style.display = "none"; //dann ist auch das tooltip.div nicht da 
+        }
+        else {
+            toolTip.style.display = "inline-block"; //wenn nicht 0 divs angeklickt sind, dann wird das tooltip.div angezeigt
+        }
+        for (var i = 0; i < roteDivs.length; i++) {
+            summe += Number(roteDivs[i].textContent); //der nummerninhat der angeklickten divs summiert sich
+            var hexa = summe.toString(16); //berechnet die hexadezimalzahl aus der summe der reisk�rner der angeklickten divs
+            var dezi = summe.toString(); //berechnet die dezimalzahl aus der summe der reisk�rner der angeklickten divs
+            toolTip.textContent = "Summe vom Reis:" + "\r\n" /* absatz */ + "Dezimal: " + dezi + "\r\n" + "Hexadezimal: " + hexa; //schreibt in das tooltip.div die ben�tigten zahlen
+        }
+    }
+    document.addEventListener("mousemove", function (Event) {
+        document.getElementById("tooltip").style.left = (Event.clientX + 10) + "px";
+        document.getElementById("tooltip").style.top = (Event.clientY + 10) + "px";
+    });
 });
 //# sourceMappingURL=Aufgabe3a.js.map
--- Aufgabe3/A/Aufgabe3a.ts
+++ Aufgabe3/A/Aufgabe3a.ts
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function(): void {
         if (i % 8 == 0) {
             zeile = zeile + 1;
         }
-        //Schwarzweiﬂ
+        //Schwarzwei√ü
         if (zeile % 2 == 0) {
             if (i % 2 != 0) {
                 element.className = "board white";
@@ -31,5 +31,35 @@ document.addEventListener("DOMContentLoaded", function(): void {
         rice = rice * 2;
         document.body.appendChild(element);
     }
+        var obereDivReihe = document.getElementsByTagName("div");
+    
+    // Gibt den Feldern in der ersten Reihe bei einem Klick die Klasse "farbe", wodurch sie rot werden
+    for (var i = 0; i < 9; i++) {
+        obereDivReihe[i].addEventListener("click", function () {
+            this.classList.toggle("farbe"); //fÔøΩrbt genau dieses angeklickte div durch die cssKlasse farbe ein
+            summeK(); // hier wird der funktion summeK bei aktivierung gesagt welches div angeklickt worden ist
+        });
+    }
+    var roteDivs = document.getElementsByClassName("farbe");
+    var summe = 0; // wird die summe aller angeklickten divs nachher ergeben 
+    var toolTip = document.getElementById("tooltip"); // div im html mit id wo die summe der angeklickten divs nacher angezeigt wird
+    function summeK() {
+        if (roteDivs.length == 0) {
+            toolTip.style.display = "none"; //dann ist auch das tooltip.div nicht da 
+        }
+        else {
+            toolTip.style.display = "inline-block"; //wenn nicht 0 divs angeklickt sind, dann wird das tooltip.div angezeigt
+        }
+        for (var i = 0; i < roteDivs.length; i++) {
+            summe += Number(roteDivs[i].textContent); //der nummerninhat der angeklickten divs summiert sich
+            var hexa = summe.toString(16); //berechnet die hexadezimalzahl aus der summe der reiskÔøΩrner der angeklickten divs
+            var dezi = summe.toString(); //berechnet die dezimalzahl aus der summe der reiskÔøΩrner der angeklickten divs
+            toolTip.textContent = "Summe vom Reis:" + "\r\n" /* absatz */ + "Dezimal: " + dezi + "\r\n" + "Hexadezimal: " + hexa; //schreibt in das tooltip.div die benÔøΩtigten zahlen
+        }
+    }
+    document.addEventListener("mousemove", function (Event) {
+        document.getElementById("tooltip").style.left = (Event.clientX + 10) + "px";
+        document.getElementById("tooltip").style.top = (Event.clientY + 10) + "px";
+    });
     
 });
