d0c458b391757a762f58c22dc4196ae8a2cdf4ae
Michelle <Michelle@Michelles-PC.hfu-open.fu.hs-furtwangen.de>
2017-04-20 16:08:23 +0200

    4

--- Aufgabe4/Aufgabe4.ts
+++ Aufgabe4/Aufgabe4.ts
@@ -13,8 +13,25 @@ namespace BLUMEN {
         console.log(crc2);
         crc2.fillStyle = "#8ad0ea";
         crc2.fillRect(0, 0, canvas.width, canvas.height);
-
-        //Wiese
+        
+         //Berg
+        crc2.beginPath();
+        crc2.moveTo(0, 50);
+        crc2.lineTo(50, 25);
+        crc2.lineTo(100, 75);
+        crc2.lineTo(200, 0);
+        crc2.lineTo(350, 75);
+        crc2.lineTo(400, 25);
+        crc2.lineTo(450, 75);
+        crc2.lineTo(575, 25);
+        crc2.lineTo(600, 50);
+        crc2.lineTo(600, 200);
+        crc2.lineTo(0, 200);
+        crc2.closePath();
+        crc2.fillStyle = "#394233";
+        crc2.fill();
+        
+          //Wiese
         crc2.beginPath();
         crc2.moveTo(0, 250);
         crc2.lineTo(600, 250);
@@ -23,6 +40,10 @@ namespace BLUMEN {
         crc2.closePath();
         crc2.fillStyle = "#34ba43";
         crc2.fill();
+        
+        
+      
+        
     }
 
 }
