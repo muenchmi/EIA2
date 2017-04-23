faf5e63a4aaa45587a4f200a0839780d091933e4
Michelle <Michelle@141.28.226.207>
2017-04-23 19:11:32 +0200

    3

--- Aufgabe3/B/Aufgabe3b.js
+++ Aufgabe3/B/Aufgabe3b.js
@@ -4,10 +4,17 @@
 //Matrikel:  254180 
 //Datum: 09.04.2017
 //    
-//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert
+//hatte hilfe von Janett St�we.
 //-->
+let deck;
+let karten;
+let hkarten;
+let ablage;
 document.addEventListener("DOMContentLoaded", function () {
-    let cart = ["Herz As",
+    deck = document.getElementById("deck");
+    deck.addEventListener("click", addhkarten);
+    let karten = ["Herz As",
         "Herz K�nig",
         "Herz Dame",
         "Herz Bube",
@@ -40,5 +47,33 @@ document.addEventListener("DOMContentLoaded", function () {
         "Kreuz 8",
         "Kreuz 7"
     ];
+    hkarten = document.getElementsByClassName("hkarten");
+    for (let x = 0; x < hkarten.length; x++) {
+        hkarten[x].addEventListener("click", addablage);
+    }
+    ablage = document.getElementById("ablage");
 });
+function addhkarten() {
+    let randomCard = karten.splice(Math.round(Math.random() * (karten.length - 1)), 1)[0];
+    let freeHand = findEmpty();
+    if (freeHand != null) {
+        freeHand.innerText = randomCard;
+    }
+    if (karten.length === 0) {
+        deck.style.display = "none";
+    }
+}
+function findEmpty() {
+    for (let i = 0; i < hkarten.length; i++) {
+        if (hkarten[i].textContent === "") {
+            return hkarten[i];
+        }
+    }
+    return null;
+}
+function addablage(_event) {
+    let element = _event.target;
+    ablage.textContent = element.textContent;
+    element.textContent = "";
+}
 //# sourceMappingURL=Aufgabe3b.js.map
--- Aufgabe3/B/Aufgabe3b.ts
+++ Aufgabe3/B/Aufgabe3b.ts
@@ -4,11 +4,16 @@
 //Matrikel:  254180 
 //Datum: 09.04.2017
 //    
-//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert
+//hatte hilfe von Janett Stüwe.
 //-->
-namespace Aufgabe3b {
-    let hkarten: NodeListOf<HTMLDivElement>;  // Zugriff auf diese div Elemente auf Hand
+    let deck: HTMLElement;
+    let karten: string[];
+    let hkarten: NodeListOf<HTMLDivElement>;
+    let ablage: HTMLDivElement;
     document.addEventListener("DOMContentLoaded", function(): void {
+        deck = document.getElementById("deck");
+        deck.addEventListener("click", addhkarten);
         let karten: string[] = ["Herz As",
             "Herz König",
             "Herz Dame",
@@ -42,36 +47,43 @@ namespace Aufgabe3b {
             "Kreuz 8",
             "Kreuz 7"
         ];
+        hkarten = <NodeListOf<HTMLDivElement>>document.getElementsByClassName("hkarten");
+        for (let x: number = 0; x < hkarten.length; x++) {
+            hkarten[x].addEventListener("click", addablage);
+        }
+        ablage = <HTMLDivElement>document.getElementById("ablage");
 
-        //(max. 5)
-        let deck: HTMLElement = document.getElementsByClassName("deck"); // HTML div Elemente
-        deck.addEventListener("click", function(): void {
-            //max 5
-            let kartenaufderhand: number = document.getElementsByClassName("hkarten");
-            if (kartenaufderhand < 5) {
-                nimmeinekarte();
-            }
-            //Karten vom Stapel in die Hand 
-            function nimmeinekarte(): void {
-                let div: HTMLDivElement = document.createElement("div");
-           document.getElementsByID("hkarten").appendChild(div);
-            //zufälligeKarte
-            let n: number = Math.round(Math.random() * karten.length);
+    });
+
+    function addhkarten(): void {
+
+        let randomCard: string = karten.splice(Math.round(Math.random() * (karten.length - 1)), 1)[0];
+        let freeHand: HTMLDivElement = findEmpty();
+        if (freeHand != null) {
+            freeHand.innerText = randomCard;
         }
 
-       //Karten weglegen
-      div.addEventListener("click", function(): void {
-         let kartenweg: string[] = [];
-         let kartenwert: string = div.innerText;
-         let kartenhaufen: number = kartenweg.push(kartenwert);
-
-         document.getElementByClassName("weg").innerText = kartenwert;
-                  //Divlöschen
-            div.parentNode.removeChild(div);
-        });
+        if (karten.length === 0) {
+            deck.style.display = "none";
+        }
     }
-//});
+    function findEmpty(): HTMLDivElement {
 
+        for (let i: number = 0; i < hkarten.length; i++) {
+            if (hkarten[i].textContent === "") { //wenn Inhalt drin ist 
+                return hkarten[i];
+            }
+        }
+
+        return null;
+    }
+
+    function addablage(_event: Event): void {
+        let element: HTMLDivElement = <HTMLDivElement>_event.target;
+        ablage.textContent = element.textContent;
+        element.textContent = "";
+
+    }
 
 
 
